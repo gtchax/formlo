691ec4fe3103ba5a00b31b9df8528b0f5d237b42
Gwinyai <gtchax@users.noreply.github.com>
2023-12-29 19:44:23 +0200

    boostratped Next app

---
+++ src/app/forms/page.tsx
@@ -0,0 +1,15 @@
+import { Input } from '@/components/ui/input'
+import React from 'react'
+
+const Forms = () => {
+  return (
+    <div className='m-24'>
+        <h1>Create form</h1>
+        <div>
+            <Input className='' />
+        </div>
+    </div>
+  )
+}
+
+export default Forms
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import Header from "@/components/header.component";
 export default async function Home() {
   return (
     <>
-      <Header />
-      <section className="bg-ct-blue-600 min-h-screen pt-20">
-        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-          <p className="text-3xl font-semibold">
-            Welcome to NextAuth with Prisma ORM and Next.js 13
-          </p>
+      <section className="">
+        <div className="ml-24">
+          <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+            Formlo | Build Beautiful Forms
+          </h1>
+          <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+            And own your data
+          </h1>
+        </div>
+        <div className="mt-8 ml-24">
+          <div className="max-w-[750px] text-lg text-muted-foreground sm:text-xl">
+            Simple form builder and form analytics
+          </div>
         </div>
       </section>
     </>
